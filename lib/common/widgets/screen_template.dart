f549e62e18898a2728e23b477972cb5a44f6440e
Arya Ashari <aryaashari100@gmail.com>
2024-03-25 02:13:20 +0700

    add screen template file

---
+++ lib/common/widgets/screen_template.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tubes_galon/common/widgets/appbar.dart';
+
+class ScreenTemplate extends StatelessWidget {
+  const ScreenTemplate({super.key, required this.title, required this.child});
+  final String title;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const CommonAppBar(title: "Obrolan"),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  child
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/common/screens/chat/chat.dart
+++ lib/features/common/screens/chat/chat.dart
@@ -1,20 +1,11 @@
 import "package:flutter/material.dart";
-import "package:flutter_tubes_galon/common/widgets/appbar.dart";
+import "package:flutter_tubes_galon/common/widgets/screen_template.dart";
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CommonAppBar(title: "Obrolan"),
-          ],
-        ),
-      ),
-    );
+    return ScreenTemplate(title: "Obrolan", child: Container());
   }
 }
