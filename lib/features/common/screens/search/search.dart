ae8c22b339c5076c3aa7d43e0a650c095e0268b2
Arya Ashari <aryaashari100@gmail.com>
2024-03-25 02:45:24 +0700

    add search bar

--- lib/features/common/screens/search/search.dart
+++ lib/features/common/screens/search/search.dart
@@ -1,21 +1,34 @@
 import "package:flutter/material.dart";
 import "package:flutter_tubes_galon/common/widgets/appbar.dart";
-
+import "package:flutter_tubes_galon/common/widgets/screen_template.dart";
+import "package:flutter_tubes_galon/utils/constants/colors.dart";
+import "package:flutter_tubes_galon/utils/constants/sizes.dart";
+import "package:iconsax/iconsax.dart";
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CommonAppBar(title: "Pencarian"),
-          ],
-        ),
-      ),
-    );
+    return Scaffold(
+        body: ScreenTemplate(
+            title: "Pencarian",
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(AppSizes.defaultSpace),
+                  child: TextField(
+                    style: Theme.of(context).textTheme.titleLarge,
+                    decoration: const InputDecoration(
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.symmetric(vertical: AppSizes.md),
+                        child: Icon(Iconsax.search_status_1),
+                      ),
+                      hintText: "Cari..."
+                    ),
+                  ),
+                ),
+              ],
+            )));
   }
 }
